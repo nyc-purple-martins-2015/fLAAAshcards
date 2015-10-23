395b3ecc64cc798cd34dcbf9eb2336375218ebe0
André & Anders Drew <pair+ajessee.cernalanders@devbootcamp.com>
2015-10-23 16:03:11 -0400

    add login logout

--- app/helpers/login_helpers.rb
+++ app/helpers/login_helpers.rb
@@ -1,8 +1,19 @@
 def logged_in?
+  session[:user_id]
 end
 
 def  current_user
+  if logged_in?
+    @user = User.find(session[:user_id])
+  else
+    nil
+  end
 end
 
-def current_username
-end
+# def current_username
+#   User.find(session[:user_id]).username
+# end
+
+# def current_user_id
+#   User.find(session[:user_id]).id
+# end
