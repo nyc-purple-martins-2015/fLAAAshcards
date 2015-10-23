a983325a51b0be16c68b1c84efba2fe23b8ee247
Lawson & Alicia <pair+sonomar.yokuba@devbootcamp.com>
2015-10-23 17:44:21 -0400

    Seed, add deck roots and controllers.

---
+++ app/controllers/decks.rb
@@ -0,0 +1,23 @@
+get '/decks' do
+  @decks = Deck.all
+  erb :'/decks/index'
+end
+
+get '/decks/:name' do
+  @deck.cards.all.each |card|
+    if card.correct
+  @deck = Deck.find_by name: params[:name]
+  if @deck.cards.all.empty?
+    # redirect to('/endgame')
+  else
+  @next_card = @deck.next_card(@temp_array)
+  erb :'/decks/show'
+end
+
+put '/decks/:name' do
+  if correct
+  @temp_array.delete!(correct_answer)
+  redirect to("/decks/#{@deck.cards}")
+  elsif incorrect
+    redirect to("/decks/#{@deck.cards}")
+end
