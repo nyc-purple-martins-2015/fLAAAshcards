e86c1371757bbce96061385f3d53a6f2e6b41af7
André & Anders Drew <pair+ajessee.cernalanders@devbootcamp.com>
2015-10-23 14:53:00 -0400

    adds password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,17 @@ class User < ActiveRecord::Base
   has_many :rounds
   has_many :guesses, :through => :rounds
   has_many :decks, :through => :rounds
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
 end
+
+
