7c74c4b3ad4d258484046a9eba4ebd13b4c46aa1
André <email": null>
2015-10-24 16:58:37 -0400

    it works.

--- app/controllers/rounds.rb
+++ app/controllers/rounds.rb
@@ -12,22 +12,24 @@ end
 get '/rounds/:id' do |id|
   @round = Round.find(id)
   @current_card = @round.next_card
-  if @current_card
-    erb :'rounds/show'
-  else
-    erb :'rounds/complete'
-  end
+  erb :'rounds/show'
 end
 
 put '/rounds/:id' do |id|
   @round = Round.find(id)
   @user_answer = params[:user][:answer]
   @card = Card.find(params[:card][:id].to_i)
+  @guess = Guess.create(round_id: id, card_id: @card.id)
 
   if @user_answer == @card.answer
     @correct = "You got the last question right!"
+    @guess.update_attributes(correct: 1)
     @current_card = @round.next_card
-    erb :'rounds/show'
+    if @current_card
+      erb :'rounds/show'
+    else
+      erb :'rounds/complete'
+    end
   else
     @incorrect = "That was wrong, here is the right answer: #{@card.answer}"
     @current_card = @round.next_card
