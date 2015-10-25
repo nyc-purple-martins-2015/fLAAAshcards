4b97193dfe8d0eca3446bcdd1bd0552e76dee1ec
Anders Drew <anders.j.drew@gmail.com>
2015-10-24 20:00:28 -0400

    adds ability to create private decks

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -43,3 +43,46 @@ get '/users/:id/new_deck' do |id|
   @user = User.find(id)
   erb :'users/new_deck'
 end
+
+post '/users/:id/new_deck' do |id|
+  @user= User.find(id)
+  @deck = Deck.create(params[:deck])
+  redirect to("/users/#{@user.id}/decks/#{@deck.id}/new_card")
+end
+
+get '/users/:id/decks/:deck_id/new_card' do
+  @user = User.find(params[:id])
+  @deck = Deck.find(params[:deck_id])
+  erb :'users/new_question'
+end
+
+post '/users/:id/decks/:deck_id/next_card' do
+    @user = User.find(params[:id])
+    @deck = Deck.find(params[:deck_id])
+    print "***************************************************"
+    p @deck
+    @card = Card.create(params[:card])
+    redirect("/users/#{@user.id}/decks/#{@deck.id}/next_card")
+end
+
+get '/users/:id/decks/:deck_id/next_card' do
+    @user = User.find(params[:id])
+    @deck = Deck.find(params[:deck_id])
+    erb :'users/next_card'
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
